20ad76d53ae008659a96120f72a6d3df4c369df8
akenproger <akenproger@gmail.com>
2022-11-01 19:20:22 +0600

    get request param with query get type

--- src/Controller/ConferenceController.php
+++ src/Controller/ConferenceController.php
@@ -3,22 +3,19 @@
 namespace App\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
 class ConferenceController extends AbstractController
 {
     #[Route('/', name: 'homepage')]
-    public function index(): Response
+    public function index(Request $request): Response
     {
-//        return $this->render('conference/index.html.twig', [
-//            'controller_name' => 'ConferenceController',
-//        ]);
-        return new Response('
-        <html>
-  <body>
-      <img src="/images/under-construction.gif" />
-   </body>
-</html>');
+        $greet = '';
+        if ($name = $request->query->get('hello')) {
+            $greet = sprintf('<h1>Hello %s!</h1>', htmlspecialchars($name));
+        }
+        return new Response($greet);
     }
 }
