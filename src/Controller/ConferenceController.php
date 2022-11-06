bf4a514be3c9770cde0e5c682a9a138fd9a16586
akenproger <akenproger@gmail.com>
2022-11-06 23:00:48 +0600

    start add database configs

--- src/Controller/ConferenceController.php
+++ src/Controller/ConferenceController.php
@@ -2,29 +2,27 @@
 
 namespace App\Controller;
 
+use phpDocumentor\Reflection\Types\This;
+use PhpParser\Node\Expr\List_;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\JsonResponse;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Config\Framework\SerializerConfig;
 
+//#[Route('/blog', name: 'blog_', requirements: ['_locale' => 'en|es|fr'])]
 class ConferenceController extends AbstractController
 {
 
-    #[Route('/hello/{username}', name: 'homepage')]
+    #[Route('/username/{username}', name: 'homepage')]
     public final function index(string $username = ""): Response
     {
         return new Response(
             $username ?
-                sprintf('<h1>Hello %s!</h1> <a href="/allProducts">All products</a>', htmlspecialchars($username))
+                sprintf('<h1>Hello %s!</h1> <a href="allProducts">All products </a>', htmlspecialchars($username))
                 : 'Empty Body!');
 
     }
-
-    #[Route('/allProducts', name: 'allProduct')]
-    public final function getAllProducts(): Response
-    {
-        return new Response('<h1>Product List</h1>');
-    }
-
 
 }
